fcf5db48f54f63e145e68821626133fa52d56b6d
Creighton Soukup <creighton@creightonsoukup.com>
2017-07-19 12:52:01 -0400

    homepage

--- src/components/app.js
+++ src/components/app.js
@@ -1,20 +1,33 @@
 import React, { Component } from 'react';
 import Logo from './logo';
+import Homepage from './homepage'
 import { CSSTransitionGroup } from 'react-transition-group'
 import _ from 'lodash';
 
 export default class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      showLoader: true
+    }
+  }
+
 
   render() {
+    const loader = _.debounce(() => {this.setState({showLoader: false})}, 3000)
+    loader()
     return (
       <div className='landing-page'>
-        <CSSTransitionGroup
-        transitionName="fade"
-        transitionEnterTimeout={300}
-        transitionLeaveTimeout={1500}
-        >
-          <Logo/>
-        </CSSTransitionGroup>
+        { this.state.showLoader &&
+          <CSSTransitionGroup
+          transitionName="fade"
+          transitionEnterTimeout={300}
+          transitionLeaveTimeout={1500}>
+            <Logo/>
+          </CSSTransitionGroup>
+        }
+        <Homepage/>
       </div>
     );
   }
--- src/components/homepage.js
+++ src/components/homepage.js
@@ -2,14 +2,15 @@ import React from 'react';
 
 const Homepage = (props) => {
   return (
-    <h1>{'Creighton Soukup'}</h1>
-    <ul>
-      <li>{'fullstack development'}</li>
-      <li>{'growth hacking'}</li>
-      <li>{'brand development'}</li>
-    </ul>
-    <h3>{'coming soon'}</h3>
-    <a
+    <div className='homepage'>
+      <h1>{'Creighton Soukup'}</h1>
+      <ul>
+        <li>{'fullstack development'}</li>
+        <li>{'growth hacking'}</li>
+        <li>{'brand development'}</li>
+      </ul>
+      <h3>{'coming soon'}</h3>
+    </div>
   )
 }
 
