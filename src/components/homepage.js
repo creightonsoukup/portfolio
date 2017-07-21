c8852900d2c5416474a5fd4083c460fafd77d123
Creighton Soukup <creighton@creightonsoukup.com>
2017-07-21 01:26:35 -0400

    homepage component structure and styling done

--- src/components/homepage.js
+++ src/components/homepage.js
@@ -3,13 +3,25 @@ import React from 'react';
 const Homepage = (props) => {
   return (
     <div className='homepage'>
-      <h1>{'Creighton Soukup'}</h1>
-      <ul>
+      <img className='homepage-section logo' src={require('../assets/mobile-c.png')}/>
+      <h1 className='homepage-section'>{'Creighton Soukup'}</h1>
+      <ul className='homepage-section'>
         <li>{'fullstack development'}</li>
         <li>{'growth hacking'}</li>
         <li>{'brand development'}</li>
       </ul>
-      <h3>{'coming soon'}</h3>
+      <h3 className='homepage-section'>{'coming soon'}</h3>
+      <div className='homepage-section'>
+        <a>contact</a>
+        <a>download resume</a>
+      </div>
+      <div className="homepage-section social">
+        <i className="fa fa-github-circle"></i>
+        <i className="fa fa-linkedin"></i>
+        <i className="fa fa-instagram-fa"></i>
+        <i className="fa fa-pinterest"></i>
+        <i className="fa fa-medium"></i>
+      </div>
     </div>
   )
 }
