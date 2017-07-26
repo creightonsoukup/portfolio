08a9b34123e5e065433a461e7b13d7a99715c24c
Creighton Soukup <creighton@creightonsoukup.com>
2017-07-26 18:46:41 -0400

    deploy envrionment

--- index.js
+++ index.js
@@ -1,12 +1,6 @@
 const express = require('express');
 const path = require('path');
-const favicon = require('serve-favicon');
-const logger = require('morgan')
-const bodyParser = require('body-parser');
-const cookieParser = require('cookie-parser');
-
 const port = process.env.PORT || 8080
-
 const app = express();
 
 app.use(express.static(__dirname));
